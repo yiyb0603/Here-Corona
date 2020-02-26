90ee90cc300da57624ee0fe4d9ad18251031f364
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-02-26 20:20:00 +0900

    [ADD] world, worldwideinfoasia

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./components/Main/Main";
 import Social from "./components/social/Social";
-import DaeguBoard from './components/Board/Daegu/DaeguBoard';
-import SeoulBoard from './components/Board/Seoul/SeoulBoard';
-import BusanBoard from './components/Board/Busan/BusanBoard';
-import NewsTemplate from './components/News/NewsTemplate';
-import Symptom from './components/Symptom/symptom';
+import DaeguBoard from "./components/Board/Daegu/DaeguBoard";
+import SeoulBoard from "./components/Board/Seoul/SeoulBoard";
+import BusanBoard from "./components/Board/Busan/BusanBoard";
+import NewsTemplate from "./components/News/NewsTemplate";
+import Symptom from "./components/Symptom/symptom";
+import WorldWide from "./components/WorldWide/WorldWide";
+import World from "./components/WorldInfoAsia/World";
 
 function App() {
   return (
@@ -17,12 +19,14 @@ function App() {
         {/* <Route exact path="/admin" component={AdminLogin} /> */}
         <Route exact path="/" component={Main} />
         <Route exact path="/social" component={Social} />
-        <Route exact path ="/DaeguBoard" component ={DaeguBoard} />
-        <Route exact path ="/SeoulBoard" component ={SeoulBoard} />
-        <Route exact path ="/BusanBoard" component ={BusanBoard} />
-        <Route exact path ="/News" component ={NewsTemplate} />
-        
-        <Route exact path ="/symptom" component={Symptom}/>
+        <Route exact path="/worldWide" component={WorldWide} />
+        <Route exact path="/worldInfoAsia" component={World} />
+        <Route exact path="/DaeguBoard" component={DaeguBoard} />
+        <Route exact path="/SeoulBoard" component={SeoulBoard} />
+        <Route exact path="/BusanBoard" component={BusanBoard} />
+        <Route exact path="/News" component={NewsTemplate} />
+
+        <Route exact path="/symptom" component={Symptom} />
       </Switch>
     </Router>
   );
