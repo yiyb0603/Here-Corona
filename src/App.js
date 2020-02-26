23d5e6eb64c57b8fc40aeee1a8c7f109d2eeea89
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-02-26 18:59:23 +0900

    [ADD]Router
    App.js파일 symptom Router 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import logo from "./logo.svg";
-import AdminLogin from "./components/Admin/AdminLogin";
+// import AdminLogin from "./components/Admin/AdminLogin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./components/Main/Main";
 import Social from "./components/social/Social";
@@ -8,18 +8,21 @@ import DaeguBoard from './components/Board/Daegu/DaeguBoard';
 import SeoulBoard from './components/Board/Seoul/SeoulBoard';
 import BusanBoard from './components/Board/Busan/BusanBoard';
 import NewsTemplate from './components/News/NewsTemplate';
+import Symptom from './components/Symptom/symptom';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/admin" component={AdminLogin} />
+        {/* <Route exact path="/admin" component={AdminLogin} /> */}
         <Route exact path="/" component={Main} />
         <Route exact path="/social" component={Social} />
         <Route exact path ="/DaeguBoard" component ={DaeguBoard} />
         <Route exact path ="/SeoulBoard" component ={SeoulBoard} />
         <Route exact path ="/BusanBoard" component ={BusanBoard} />
         <Route exact path ="/News" component ={NewsTemplate} />
+        
+        <Route exact path ="/symptom" component={Symptom}/>
       </Switch>
     </Router>
   );
--- src/components/Symptom/symptom.js
+++ src/components/Symptom/symptom.js
@@ -3,7 +3,11 @@ import React from 'react';
 import {Link, WithRouter} from 'react-router-dom';
 
 const Symptom =()=> {
-    
+    return(
+        <>
+        <div>안녕</div>
+        </>
+    )
 }
 
 export default Symptom; 
