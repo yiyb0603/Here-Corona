0a45fb12509f6a9f4fdbaf46ed4d3e2166f4d75c
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-02-26 19:10:07 +0900

    [ADD]symptom
    의심 증상 스타일링 전 요소

--- src/components/Symptom/symptom.js
+++ src/components/Symptom/symptom.js
@@ -5,7 +5,15 @@ import {Link, WithRouter} from 'react-router-dom';
 const Symptom =()=> {
     return(
         <>
-        <div>안녕</div>
+        <div className="symptom-title">신종 코로나 바이러스 의심 증상</div>
+        <div className="symptom-MainImage">image</div>
+        <div className="symptom-list">
+            <li>숨 쉬는게 힘들 정도의 마른 기침</li>
+            <li>37.5도 이상의 발열</li>
+            <li>음식 섭취 시 아픈 목</li>
+            <li>구토와 설사, 복통</li>
+            <li>온몸이 뻐근하고 아픈 근육통</li>
+        </div>
         </>
     )
 }
