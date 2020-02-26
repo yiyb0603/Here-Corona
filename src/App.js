6bf3ce5a10cd6031245f4d46bb8f991a2d915712
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-02-26 16:50:09 +0900

    [ADD] route path main/ social

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 // import logo from "./logo.svg";
 import AdminLogin from "./components/Admin/AdminLogin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Sample from "components/Navbar/sample";
+import Main from "./components/Main/Main";
+import Social from "./components/social/Social";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/admin" component={AdminLogin} />
-        <Sample />
+        <Route exact path="/" component={Main} />
+        <Route exact path="/social" component={Social} />
       </Switch>
     </Router>
   );
