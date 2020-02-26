a8747bacb5b97f5b0ee00d1c086bd898064880aa
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-02-26 16:25:05 +0900

    [ADD] 서버주소
    절대경로 안되는거도 다시 했습니다 /src

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 // import logo from "./logo.svg";
-import AdminLogin from './components/Admin/AdminLogin';
+import AdminLogin from "./components/Admin/AdminLogin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Sample from './components/Navbar/sample';
+import Sample from "components/Navbar/sample";
 
 function App() {
   return (
     <Router>
       <Switch>
-        {/* <Route exact path="/admin" component={AdminLogin} /> */}
+        <Route exact path="/admin" component={AdminLogin} />
         <Sample />
       </Switch>
     </Router>
