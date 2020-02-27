2bb0176df7dfb31516a66bce87b541e397f1b117
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-02-27 19:57:41 +0900

    [ADD] 오타 때문에 오류나는거
    c->C

--- src/containers/AdminContainer/Admin.js
+++ src/containers/AdminContainer/Admin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AdminPage from "./AdminPage/AdminPageContainer";
+import AdminPage from './AdminPage/AdminPageContainer';
 import AdminPageBar from "./AdminPageBar/AdminPageBarContainer";
 
 function Admin() {
