af45f79871f46d0236f734febb995fc4f48ad713
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-02-27 16:41:17 +0900

    [ADD] 어드민 페이지 추가

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import AdminLogin from "containers/AdminContainer/AdminLogin/AdminLoginContainer
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Main/Main";
 import Social from "./social/Social";
-import BoardInsert from './Board/BoardInsert';
+import BoardInsert from "./Board/BoardInsert";
 import DaeguBoard from "./Board/Daegu/DaeguBoard";
 import SeoulBoard from "./Board/Seoul/SeoulBoard";
 import BusanBoard from "./Board/Busan/BusanBoard";
@@ -12,6 +12,7 @@ import Symptom from "./Symptom/symptom";
 import Market from "./Market/market";
 import WorldWide from "./WorldWide/WorldWide";
 import World from "./WorldInfoAsia/World";
+import AdminPage from "components/Admin/AdminPage";
 
 function App() {
   return (
@@ -22,13 +23,14 @@ function App() {
         <Route exact path="/social" component={Social} />
         <Route exact path="/worldWide" component={WorldWide} />
         <Route exact path="/worldInfoAsia" component={World} />
-        <Route exact path ="/BoardInsert" component ={BoardInsert} />
+        <Route exact path="/BoardInsert" component={BoardInsert} />
         <Route exact path="/DaeguBoard" component={DaeguBoard} />
         <Route exact path="/SeoulBoard" component={SeoulBoard} />
         <Route exact path="/BusanBoard" component={BusanBoard} />
         <Route exact path="/News" component={NewsTemplate} />
         <Route exact path="/symptom" component={Symptom} />
-        <Route exact path="/market" cmoponent={Market} />
+        <Route exact path="/market" component={Market} />
+        <Route exact path="/adminpage" component={AdminPage} />
       </Switch>
     </Router>
   );
