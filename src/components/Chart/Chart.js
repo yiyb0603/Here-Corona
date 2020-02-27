92a210cd3d01985ba0fa57288c67494b805e7f84
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-02-27 20:27:14 +0900

    [ADD] 차트 서버에서 받은 값 추가

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,38 +1,91 @@
 import React from "react";
 import { ColumnChart } from "@opd/g2plot-react";
+//
+import axios from "axios";
+import { SERVER } from "config/server.json";
+import { Component } from "react";
 
-const config = {
-  title: {
-    visible: true,
-    text: "국내 확진자 누적 그래프"
-  },
-  forceFit: true,
-  data: [
-    {
-      type: "2.25",
-      sales: 997
-    },
-    {
-      type: "2.26",
-      sales: 1146
-    }
-  ],
-  padding: "auto",
-  color: "#e53a40",
-  xField: "type",
-  yField: "sales",
-  meta: {
-    type: {
-      alias: "날짜"
-    },
-    sales: {
-      alias: "국내 확진자"
-    }
+// let date = async () => {
+//   await axios.get(`${SERVER}/api/infectee/date`).then(response => {
+//     console.log(response.data.data.date_infectee);
+//     return ({ data } = response.data.data.date_infectee);
+//   });
+// };
+// date();
+// const config = {
+//   title: {
+//     visible: true,
+//     text: "국내 확진자 누적 그래프"
+//   },
+//   forceFit: true,
+//   data: this.getinfectee.data,
+//   padding: "auto",
+//   color: "#e53a40",
+//   xField: "type",
+//   yField: "sales",
+//   meta: {
+//     type: {
+//       alias: "날짜"
+//     },
+//     sales: {
+//       alias: "국내 확진자"
+//     }
+//   }
+// };
+
+// export default () => (
+//   <section>
+//     <ColumnChart {...config} />
+//   </section>
+// );
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      getArray: ""
+    };
   }
-};
+  componentDidMount() {
+    let date = async () => {
+      await axios.get(`${SERVER}/api/infectee/date`).then(response => {
+        console.log(response.data.data.date_infectee);
+        this.setState({
+          getArray: response.data.data.date_infectee
+        });
+      });
+    };
+    date();
+    // console.log(getArray);
+  }
+  render() {
+    // date();
+    const config = {
+      title: {
+        visible: true,
+        text: "국내 확진자 누적 그래프"
+      },
+      forceFit: true,
+      data: this.state.getArray,
+      padding: "auto",
+      color: "#e53a40",
+      xField: "date",
+      yField: "number",
+      meta: {
+        date: {
+          alias: "날짜"
+        },
+        number: {
+          alias: "국내 확진자"
+        }
+      }
+    };
+    console.log(this.state.getArray);
+    return (
+      <section>
+        <ColumnChart {...config} />
+      </section>
+    );
+  }
+}
 
-export default () => (
-  <section>
-    <ColumnChart {...config} />
-  </section>
-);
+export default Chart;
--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -27,89 +27,94 @@ class Social extends Component {
   render() {
     return (
       <>
-        <div className="allType">
-          <div class="wrapper">
-            <nav id="sidebar">
-              <div class="sidebar-header">
-                <h3>메로나, 여기</h3>
-              </div>
+        <div className="SocialType">
+          <div className="allType">
+            <div class="wrapper">
+              <nav id="sidebar">
+                <div class="sidebar-header">
+                  <h3>메로나, 여기</h3>
+                </div>
 
-              <ul class="list-unstyled components">
-                <p>Corona?</p>
-                <li class="active">
-                  <a
-                    data-toggle="collapse"
-                    aria-expanded="false"
-                    onClick={() => {
-                      this.props.history.push("/");
-                    }}
-                  >
-                    국내현황
-                  </a>
-                </li>
-                <li>
-                  <a
-                    onClick={() => {
-                      this.props.history.push("/worldWide");
-                    }}
-                  >
-                    전세계 현황
-                  </a>
-                  <a data-toggle="collapse" aria-expanded="false">
-                    국내 사망자 현황
-                  </a>
-                </li>
-                <li>
-                  <a>격리 해제 현황</a>
-                </li>
-                <li>
-                  <a>지역 공유 게시판</a>
-                </li>
-                <li>
-                  <a>의심 증상</a>
-                </li>
-                <li>
-                  <a>자영업자 장터</a>
-                </li>
-                <li>
-                  <a>문의 질문</a>
-                </li>
-              </ul>
-              <ul class="list-unstyled CTAs">
-                <li>
-                  <a>지역 콜센터 120</a>
-                </li>
-                <li>
-                  <a>콜센터 1339</a>
-                </li>
-              </ul>
-            </nav>
+                <ul class="list-unstyled components">
+                  <p>Corona?</p>
+                  <li class="active">
+                    <a
+                      data-toggle="collapse"
+                      aria-expanded="false"
+                      onClick={() => {
+                        this.props.history.push("/");
+                      }}
+                    >
+                      국내현황
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => {
+                        this.props.history.push("/worldWide");
+                      }}
+                    >
+                      전세계 현황
+                    </a>
+                    <a data-toggle="collapse" aria-expanded="false">
+                      국내 사망자 현황
+                    </a>
+                  </li>
+                  <li>
+                    <a>격리 해제 현황</a>
+                  </li>
+                  <li>
+                    <a>지역 공유 게시판</a>
+                  </li>
+                  <li>
+                    <a>의심 증상</a>
+                  </li>
+                  <li>
+                    <a>자영업자 장터</a>
+                  </li>
+                  <li>
+                    <a>문의 질문</a>
+                  </li>
+                </ul>
+                <ul class="list-unstyled CTAs">
+                  <li>
+                    <a>지역 콜센터 120</a>
+                  </li>
+                  <li>
+                    <a>콜센터 1339</a>
+                  </li>
+                </ul>
+              </nav>
 
-            <div id="content">
-              <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                  <button type="button" id="sidebarCollapse" class="navbar-btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </button>
-                  <button
-                    class="btn btn-dark d-inline-block d-lg-none ml-auto"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                  >
-                    <i class="fas fa-align-justify"></i>
-                  </button>
+              <div id="content">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                  <div class="container-fluid">
+                    <button
+                      type="button"
+                      id="sidebarCollapse"
+                      class="navbar-btn"
+                    >
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </button>
+                    <button
+                      class="btn btn-dark d-inline-block d-lg-none ml-auto"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                    >
+                      <i class="fas fa-align-justify"></i>
+                    </button>
 
-                  <div
-                    class="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                  >
-                    {/* <ul class="nav navbar-nav ml-auto">
+                    <div
+                      class="collapse navbar-collapse"
+                      id="navbarSupportedContent"
+                    >
+                      {/* <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item active">
                       <a class="nav-link">
                         Page
@@ -131,12 +136,13 @@ class Social extends Component {
                       </a>
                     </li>
                   </ul> */}
+                    </div>
                   </div>
-                </div>
-              </nav>
-              여기 지도
-              <Circle />
-              <AgeChart />
+                </nav>
+                여기 지도
+                <Circle />
+                <AgeChart />
+              </div>
             </div>
           </div>
         </div>
