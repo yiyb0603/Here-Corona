c72e84a98f84e24cdc749e5749ad18b1e3930f59
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-02-28 15:07:46 +0900

    [ADD&FIX]market,market list
    자영업자 장터 스타일링 설정, 자영업자 장터 리스트 아이템 파일 추가

---
+++ src/components/Market/MarketList/marketList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MarketList =()=> {
+    return(
+        <>
+        <div>
+            리스트 아이템 입니다
+        </div>
+        </>
+    )
+}
+
+export default MarketList;
--- src/components/Market/market.js
+++ src/components/Market/market.js
@@ -1,19 +1,25 @@
 //자영업자 장터
 import React from 'react';
+
+import MarketList from 'components/Market/MarketList/marketList';
 import { FaBars} from 'react-icons/fa';
+import 'components/Market/market.scss';
 
 const Market = () => {
     return(
         <>
             <div className="market-title">
-                <FaBars/>
                 <input type="text" className="market-input"/>
                 <span>자영업자 장터</span>
             </div>
 
             <div className="market-Notice">
                 <span>공지</span>
-                <span></span>
+                <span>공지사항 내용 들어가는 부분 입니다.</span>
+            </div>
+
+            <div className="market-list">
+                <MarketList/>
             </div>
         </>
     )
