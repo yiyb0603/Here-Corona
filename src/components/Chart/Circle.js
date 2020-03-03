e6805385fdbe13e557eb58e057761b075763e4df
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-03-03 16:20:25 +0900

    Update Circle.js

--- src/components/Chart/Circle.js
+++ src/components/Chart/Circle.js
@@ -1,40 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { PieChart } from "@opd/g2plot-react";
 
-const data = [
-  {
-    type: "남성",
-    value: 343
-  },
-  {
-    type: "여성",
-    value: 550
+class Circle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      getArrayMin: []
+    };
   }
-];
+  render() {
+    const data = [
+      {
+        type: "남성",
+        value: 343
+      },
+      {
+        type: "여성",
+        value: 550
+      }
+    ];
 
-const config = {
-  forceFit: true,
-  title: {
-    visible: true,
-    text: "메로나"
-  },
-  description: {
-    visible: true,
-    text: "확진자 성비"
-  },
-  radius: 0.8,
-  padding: "auto",
-  data,
-  color: ["#05C2AB", "#FF5152"],
-  angleField: "value",
-  colorField: "type",
-  statistic: {
-    visible: true
+    const config = {
+      forceFit: true,
+      title: {
+        visible: true,
+        text: "메로나"
+      },
+      description: {
+        visible: true,
+        text: "확진자 성비"
+      },
+      radius: 0.8,
+      padding: "auto",
+      data,
+      color: ["#05C2AB", "#FF5152"],
+      angleField: "value",
+      colorField: "type",
+      statistic: {
+        visible: true
+      }
+    };
+    return (
+      <>
+        <div>
+          <section>
+            <PieChart {...config} />
+          </section>
+        </div>
+      </>
+    );
   }
-};
+}
 
-export default () => (
-  <section>
-    <PieChart {...config} />
-  </section>
-);
+export default Circle;
