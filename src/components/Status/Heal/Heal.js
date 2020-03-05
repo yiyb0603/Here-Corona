14eef9c986476fbb679266f0f84913f06ec6bc98
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-03-05 21:57:12 +0900

    [ADD] 격리해제자 표

--- src/components/Status/Heal/Heal.js
+++ src/components/Status/Heal/Heal.js
@@ -163,59 +163,253 @@ class Heal extends Component {
                 <div
                   class="collapse navbar-collapse"
                   id="navbarSupportedContent"
-                >
-                  {/* <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">
-                        Page
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">
-                        Page
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">
-                        Page
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">
-                        Page
-                      </a>
-                    </li>
-                  </ul> */}
-                </div>
+                ></div>
               </div>
             </nav>
-            {/* <div className="twoBoxalign"> */}
             <Table striped bordered hover>
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>환자번호</th>
+                  <th>출생년도</th>
+                  <th>성별</th>
+                  <th>확진일</th>
+                  <th>격리해제일</th>
+                  <th>감염경로</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                  <td>#1</td>
+                  <td>1984(36)</td>
+                  <td>여</td>
+                  <td>01.20</td>
+                  <td>02.06</td>
+                  <td>우한방문(1차)</td>
                 </tr>
                 <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
+                  <td>#2</td>
+                  <td>1964(56)</td>
+                  <td>남</td>
+                  <td>01.24</td>
+                  <td>02.05</td>
+                  <td>우한방문(1차)</td>
                 </tr>
                 <tr>
-                  <td>3</td>
-                  <td colSpan="2">Larry the Bird</td>
-                  <td>@twitter</td>
+                  <td>#3</td>
+                  <td>1966(54)</td>
+                  <td>남</td>
+                  <td>01.26</td>
+                  <td>02.12</td>
+                  <td>우한방문(1차)</td>
+                </tr>
+
+                <tr>
+                  <td>#4</td>
+                  <td>1964(56)</td>
+                  <td>남</td>
+                  <td>01.27</td>
+                  <td>02.09</td>
+                  <td>우한방문(1차)</td>
+                </tr>
+
+                <tr>
+                  <td>#6</td>
+                  <td>1964(56)</td>
+                  <td>남</td>
+                  <td>01.30</td>
+                  <td>02.19</td>
+                  <td>3번째 환자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#7</td>
+                  <td>1991(29)</td>
+                  <td>남</td>
+                  <td>01.31</td>
+                  <td>02.15</td>
+                  <td>우한방문(1차)</td>
+                </tr>
+
+                <tr>
+                  <td>#8</td>
+                  <td>1957(63)</td>
+                  <td>여</td>
+                  <td>01.31</td>
+                  <td>02.11</td>
+                  <td>우한방문(1차)</td>
+                </tr>
+
+                <tr>
+                  <td>#9</td>
+                  <td>1992(28)</td>
+                  <td>여</td>
+                  <td>01.31</td>
+                  <td>02.24</td>
+                  <td>5번째 환자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#10</td>
+                  <td>1966(54)</td>
+                  <td>여</td>
+                  <td>01.31</td>
+                  <td>02.19</td>
+                  <td>6번째 환자(3번째 환자 접촉자) 배우자(3차)</td>
+                </tr>
+
+                <tr>
+                  <td>#11</td>
+                  <td>1995(25)</td>
+                  <td>남</td>
+                  <td>01.31</td>
+                  <td>02.01</td>
+                  <td>
+                    6번째 환자(3번째 환자 접촉자) 접촉자(3차) 6번 확진자의 아들
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>#12</td>
+                  <td>1971(49)</td>
+                  <td>남</td>
+                  <td>02.01</td>
+                  <td>02.18</td>
+                  <td>일본 내 확진환자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#13</td>
+                  <td>1992(28)</td>
+                  <td>남</td>
+                  <td>02.02</td>
+                  <td>02.24</td>
+                  <td>우한방문(1차):우한 교민</td>
+                </tr>
+
+                <tr>
+                  <td>#14</td>
+                  <td>1980(40)</td>
+                  <td>여</td>
+                  <td>02.02</td>
+                  <td>02.18</td>
+                  <td>12번째 환자(일본 내 확진환자 접촉자)배우자 (3차)</td>
+                </tr>
+
+                <tr>
+                  <td>#15</td>
+                  <td>1977(43)</td>
+                  <td>남</td>
+                  <td>02.02</td>
+                  <td>02.24</td>
+                  <td>4번째 확진자 접촉자(항공기) (2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#16</td>
+                  <td>1977(43)</td>
+                  <td>여</td>
+                  <td>02.04</td>
+                  <td>02.19</td>
+                  <td>태국여행(1차)</td>
+                </tr>
+
+                <tr>
+                  <td>#17</td>
+                  <td>1982(38)</td>
+                  <td>남</td>
+                  <td>02.05</td>
+                  <td>02.11</td>
+                  <td>싱가포르 컨퍼런스 내 확진자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#18</td>
+                  <td>1999(21)</td>
+                  <td>여</td>
+                  <td>02.05</td>
+                  <td>02.19</td>
+                  <td>16번 확진자의 딸(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#19</td>
+                  <td>1983(37)</td>
+                  <td>남</td>
+                  <td>02.05</td>
+                  <td>02.21</td>
+                  <td>싱가포르 컨퍼런스 내 확진자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#20</td>
+                  <td>1978(42)</td>
+                  <td>여</td>
+                  <td>02.05</td>
+                  <td>02.24</td>
+                  <td>15번째 확진자(4번째 확진자 접촉자)의 처제(3차)</td>
+                </tr>
+
+                <tr>
+                  <td>#22</td>
+                  <td>1973(47)</td>
+                  <td>남</td>
+                  <td>02.06</td>
+                  <td>02.15</td>
+                  <td>16번째 확진자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#25</td>
+                  <td>1946(74)</td>
+                  <td>여</td>
+                  <td>02.09</td>
+                  <td>02.22</td>
+                  <td>27번째 확진자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#28</td>
+                  <td>1989(31)</td>
+                  <td>여</td>
+                  <td>02.10</td>
+                  <td>02.17</td>
+                  <td>3번째 확진자 접촉자(2차)</td>
+                </tr>
+
+                <tr>
+                  <td>#37</td>
+                  <td>1973(47)</td>
+                  <td>남</td>
+                  <td>02.19</td>
+                  <td>02.26</td>
+                  <td></td>
+                </tr>
+
+                <tr>
+                  <td>#51</td>
+                  <td>1959(61)</td>
+                  <td>여</td>
+                  <td>02.19</td>
+                  <td>02.26</td>
+                  <td></td>
+                </tr>
+
+                <tr>
+                  <td>#24</td>
+                  <td>1992(28)</td>
+                  <td>남</td>
+                  <td>02.06</td>
+                  <td>02.27</td>
+                  <td>우한방문(1차)</td>
+                </tr>
+
+                <tr>
+                  <td>#65</td>
+                  <td>1970(50)</td>
+                  <td>여</td>
+                  <td>02.19</td>
+                  <td>02.27</td>
+                  <td></td>
                 </tr>
               </tbody>
             </Table>
