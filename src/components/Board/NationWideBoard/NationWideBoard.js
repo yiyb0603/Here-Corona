cb91edc0fab1667e58d4d0aa100f544471abc1a8
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-03-07 22:51:21 +0900

    update

--- src/components/Board/NationWideBoard/NationWideBoard.js
+++ src/components/Board/NationWideBoard/NationWideBoard.js
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import './NationWideBoard.scss';
-import { FaBars, FaPhabricator, FaRegCommentAlt } from 'react-icons/fa';
-import { withRouter, Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./NationWideBoard.scss";
+import { FaBars, FaPhabricator, FaRegCommentAlt } from "react-icons/fa";
+import { withRouter, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
-import './NationWideNav.scss'
+import "./NationWideNav.scss";
 
 /* eslint-disable */
 
-const NationWideBoard = ({ history, nationList, requestTimeList, popularNationWideList }) => {
+const NationWideBoard = ({
+  history,
+  nationList,
+  requestTimeList,
+  popularNationWideList
+}) => {
   const [isTimeOrder, setIsTimeOrder] = useState(false);
   let nationItem;
-    useEffect(() => {
-        $(document).ready(function() {
-            $("#sidebarCollapse").on("click", function() {
-              $("#sidebar").toggleClass("active");
-              $(this).toggleClass("active");
-            });
-          });
-    }, []);
+  useEffect(() => {
+    $(document).ready(function() {
+      $("#sidebarCollapse").on("click", function() {
+        $("#sidebar").toggleClass("active");
+        $(this).toggleClass("active");
+      });
+    });
+  }, []);
 
-    if (!isTimeOrder) {
+  if (!isTimeOrder) {
     nationItem = nationList.map((data, index) => {
       let { idx, title, region, view, created_at } = data;
       return (
@@ -51,9 +56,7 @@ const NationWideBoard = ({ history, nationList, requestTimeList, popularNationWi
         </div>
       );
     });
-  }
-
-  else if (isTimeOrder) {
+  } else if (isTimeOrder) {
     nationItem = popularNationWideList.map((data, index) => {
       let { idx, title, region, view, created_at } = data;
       return (
@@ -86,150 +89,150 @@ const NationWideBoard = ({ history, nationList, requestTimeList, popularNationWi
     });
   }
 
-    return (
-        <>
-        <div className="NationWideNav">
-          <div class="wrapper">
-            <nav id="sidebar">
-              <div class="sidebar-header">
-                <h3>코로나, 여기</h3>
-              </div>
+  return (
+    <>
+      <div className="NationWideNav">
+        <div class="wrapper">
+          <nav id="sidebar">
+            <div class="sidebar-header">
+              <h3>코로나, 여기</h3>
+            </div>
 
-              <ul class="list-unstyled components">
-                <p>Corona?</p>
-                <li>
-                  <a
-                    data-toggle="collapse"
-                    aria-expanded="false"
-                    onClick={() => {
-                      history.push("/");
-                    }}
-                  >
-                    국내현황
-                  </a>
-                </li>
-                <li>
-                  <a
-                    onClick={() => {
-                      history.push("/worldWide");
-                    }}
-                  >
-                    전세계 현황
-                  </a>
-
-                  <a
-                    onClick={() => {
-                      history.push("/passed");
-                    }}
-                  >
-                    국내 사망자 현황
-                  </a>
-                </li>
-                <li>
-                  <a
-                    onClick={() => {
-                      history.push("/healed");
-                    }}
-                  >
-                    국내 격리 해제 현황
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#pageSubmenu"
-                    data-toggle="collapse"
-                    aria-expanded="false"
-                    class="dropdown-toggle"
-                  >
-                    지역 공유 게시판
-                  </a>
-                  <ul class="collapse list-unstyled" id="pageSubmenu">
-                    <li
-                      onClick={() => {
-                        history.push("/NationWideBoard");
-                      }}
-                    >
-                      <a>전국</a>
-                    </li>
-                    <li
-                      onClick={() => {
-                        history.push("/DaeguBoard");
-                      }}
-                    >
-                      <a>대구</a>
-                    </li>
-                    <li
-                      class="active"
-                      onClick={() => {
-                        history.push("/BusanBoard");
-                      }}
-                    >
-                      <a>부산</a>
-                    </li>
-                    <li
-                      onClick={() => {
-                        history.push("/SeoulBoard");
-                      }}
-                    >
-                      <a>서울</a>
-                    </li>
-                  </ul>
-                </li>
-                <li
+            <ul class="list-unstyled components">
+              <p>Corona?</p>
+              <li>
+                <a
+                  data-toggle="collapse"
+                  aria-expanded="false"
                   onClick={() => {
-                    history.push("/symptom");
+                    history.push("/");
                   }}
                 >
-                  <a>의심 증상</a>
-                </li>
-                <li>
-                  <a
+                  국내현황
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    history.push("/worldWide");
+                  }}
+                >
+                  전세계 현황
+                </a>
+
+                <a
+                  onClick={() => {
+                    history.push("/passed");
+                  }}
+                >
+                  국내 사망자 현황
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    history.push("/healed");
+                  }}
+                >
+                  국내 격리 해제 현황
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#pageSubmenu"
+                  data-toggle="collapse"
+                  aria-expanded="false"
+                  class="dropdown-toggle"
+                >
+                  지역 공유 게시판
+                </a>
+                <ul class="collapse list-unstyled" id="pageSubmenu">
+                  <li
                     onClick={() => {
-                      this.props.history.push("/market");
+                      history.push("/NationWideBoard");
                     }}
                   >
-                    자영업자 장터
-                  </a>
-                </li>
-                <li>
-                  <a href="#">문의/질문</a>
-                </li>
-              </ul>
-              <ul class="list-unstyled CTAs">
-                <li>
-                  <a>지역 콜센터 120</a>
-                </li>
-                <li>
-                  <a>콜센터 1339</a>
-                </li>
-              </ul>
-            </nav>
-
-            <div id="content">
-              <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                  <button type="button" id="sidebarCollapse" class="navbar-btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </button>
-                  <button
-                    class="btn btn-dark d-inline-block d-lg-none ml-auto"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
+                    <a>전국</a>
+                  </li>
+                  <li
+                    onClick={() => {
+                      history.push("/DaeguBoard");
+                    }}
                   >
-                    <i class="fas fa-align-justify"></i>
-                  </button>
-
-                  <div
-                    class="collapse navbar-collapse"
-                    id="navbarSupportedContent"
+                    <a>대구</a>
+                  </li>
+                  <li
+                    class="active"
+                    onClick={() => {
+                      history.push("/BusanBoard");
+                    }}
                   >
-                    {/* <ul class="nav navbar-nav ml-auto">
+                    <a>부산</a>
+                  </li>
+                  <li
+                    onClick={() => {
+                      history.push("/SeoulBoard");
+                    }}
+                  >
+                    <a>서울</a>
+                  </li>
+                </ul>
+              </li>
+              <li
+                onClick={() => {
+                  history.push("/symptom");
+                }}
+              >
+                <a>의심 증상</a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    history.push("/market");
+                  }}
+                >
+                  자영업자 장터
+                </a>
+              </li>
+              <li>
+                <a href="#">문의/질문</a>
+              </li>
+            </ul>
+            <ul class="list-unstyled CTAs">
+              <li>
+                <a>지역 콜센터 120</a>
+              </li>
+              <li>
+                <a>콜센터 1339</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div id="content">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <div class="container-fluid">
+                <button type="button" id="sidebarCollapse" class="navbar-btn">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+                <button
+                  class="btn btn-dark d-inline-block d-lg-none ml-auto"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <i class="fas fa-align-justify"></i>
+                </button>
+
+                <div
+                  class="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  {/* <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item active">
                       <a class="nav-link" href="#">
                         Page
@@ -251,42 +254,48 @@ const NationWideBoard = ({ history, nationList, requestTimeList, popularNationWi
                       </a>
                     </li>
                   </ul> */}
-                  </div>
                 </div>
-              </nav>
+              </div>
+            </nav>
 
-        <div className ="NationWideBoard">
-            <div className="NationWideBoard-TitleZone">
-                <span className="NationWideBoard-Title">전국 정보 공유 게시판</span>
-            </div>
+            <div className="NationWideBoard">
+              <div className="NationWideBoard-TitleZone">
+                <span className="NationWideBoard-Title">
+                  전국 정보 공유 게시판
+                </span>
+              </div>
 
-            <div className="Board-Button">
+              <div className="Board-Button">
                 <button
                   className="Board-Button-Button"
                   onClick={() => (window.location = "/BoardInsert")}
                 >
-                글쓰기
+                  글쓰기
                 </button>
-                <button className="Board-Button-Button" onClick ={() => {
-                    requestTimeList(),
-                    setIsTimeOrder(!isTimeOrder);
-                  }}>{
-                    isTimeOrder ? <span>인기순</span> : <span>최신순</span>
-                  }</button>
-                </div>
+                <button
+                  className="Board-Button-Button"
+                  onClick={() => {
+                    requestTimeList(), setIsTimeOrder(!isTimeOrder);
+                  }}
+                >
+                  {isTimeOrder ? <span>인기순</span> : <span>최신순</span>}
+                </button>
+              </div>
 
-            <div className="NationWideBoard-NoticeZone">
+              <div className="NationWideBoard-NoticeZone">
                 <span className="NationWideBoard-Notice">공지</span>
-                <span className="NationWideBoard-NoticeContents">익명 게시판입니다. 비방, 욕설 등은 삼가해주세요.</span>
-            </div>
+                <span className="NationWideBoard-NoticeContents">
+                  익명 게시판입니다. 비방, 욕설 등은 삼가해주세요.
+                </span>
+              </div>
 
-            {nationItem}
+              {nationItem}
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default withRouter(NationWideBoard);
